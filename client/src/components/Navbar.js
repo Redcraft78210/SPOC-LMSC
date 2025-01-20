91198be279a6837b348856602d4263214e6daea7
Redcraft78210 <belaise.clement@gmail.com>
2025-01-20 11:24:30 +0100

    test clear navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,45 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Navbar, Nav, Button, Collapse } from 'react-bootstrap';
 
-function Navbar() {
+const VerticalNavbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ width: "300px", height: "2000px" }}>
-      <Link className="navbar-brand" to="/">SPOC</Link>
-      <button 
-        className="navbar-toggler" 
-        type="button" 
-        data-bs-toggle="collapse" 
-        data-bs-target="#navbarNav"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <div className="navbar-square-first bg-light"></div>
-        <div className="navbar-square-second"></div>
-        <div className="n_simple container-fluid">
-          <div className="links-container n_links-container">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="n_link-button nav-link d-block text-center bg-white text-primary text-decoration-none rounded-circle fw-bold" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="n_link-button nav-link d-block text-center bg-white text-primary text-decoration-none rounded-circle fw-bold" to="/courses">Courses</Link>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="n_link-button link-button d-block text-center bg-white text-primary text-decoration-none rounded-circle fw-bold">Contact</a>
-              </li>
-              <li className="nav-item">
-                <Link className="n_link-button nav-link d-block text-center bg-white text-primary text-decoration-none rounded-circle fw-bold" to="/profile">Profile</Link>
-              </li>
-            </ul>
+    <div className="d-flex">
+      {/* Vertical Navbar */}
+      <div>
+        <Button
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+          className="mb-3"
+        >
+          Toggle Menu
+        </Button>x
+        <Collapse in={open}>
+          <div id="example-collapse-text" className="bg-light border p-3" style={{ width: '200px' }}>
+            <Nav className="flex-column">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#services">Services</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
           </div>
-        </div>
-        <div className="navbar-square-last">
-          <div className="navbar-square-third bg-light"></div>
-        </div>
+        </Collapse>
       </div>
-    </nav>
-  );
-}
 
-export default Navbar;
+      {/* Main Content */}
+      <div className="flex-grow-1 p-3">
+        <h1>Main Content</h1>
+        <p>This is where the main content of the page would go.</p>
+      </div>
+    </div>
+  );
+};
+
+export default VerticalNavbar;
