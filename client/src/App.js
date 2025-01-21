db7847f23f69aa004927639ca544327f8c197be8
RRTracer onGithub <rrtracer@gmail.com>
2025-01-21 16:06:12 +0100

    Mise en place de ma branch prêt afin de recommencer a dev proprement

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 // Components
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Courses from './pages/Courses';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
 
@@ -14,13 +12,11 @@ function App() {
     <Router>
       <div>
         {/* Navigation Bar */}
-        <Navbar />
 
         {/* Main Content */}
         <div className="container mt-4">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/courses" element={<Courses />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
