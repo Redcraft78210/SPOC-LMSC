d1263d3f2a650acb9038949198acd03ca1ea62f7
RRTracer onGithub <rrtracer@gmail.com>
2025-02-04 14:32:41 +0100

    mise a jour de la plateforme

--- client/src/pages/DashboardHome.js
+++ client/src/pages/DashboardHome.js
@@ -58,7 +58,7 @@ function DashboardHome() {
       </div>
 
       {/* Notification Card */}
-      <div className="row col-2 col-md-4 col-lg-3">
+      <div className="row col-2 col-md-6 col-lg-3">
         <NotificationCard content={<p>PLACEHOLDER</p>} />
       </div>
     </div>
