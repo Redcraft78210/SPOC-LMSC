20fb68fd1b15387a45a4429ec9ad2c4fbcaf9bb7
Redcraft78210 <belaise.clement@gmail.com>
2025-02-06 13:51:27 +0100

    Ajout du module de déconnexion, du contrôle d'accès et du mode sombre/clair

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
-import NavigationBar from './components/NavigationBar'
+import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Sign from './pages/Sign';
+import Logout from './components/Logout';
 import NotFound from './pages/NotFound';
 
 const routeConfig = [
   { path: '/', content: 'Home' },
+  { path: '/dashboard', content: 'Home' },
   { path: '/profile', content: 'Profile' },
   { path: '/courses-library', content: 'Courses' },
 
@@ -16,16 +18,76 @@ const routeConfig = [
 
 
 function App() {
+
+  const [auth, setAuth] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isfirstAuth, setFirstAuth] = useState(false);
+  const [isLoggedOut, setIsLoggedOut] = useState(false); // Update state to handle logout
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = localStorage.getItem('authToken');
+      if (token) {
+        try {
+          const decodedToken = jwtDecode(token);
+          let clientTime = new Date().getTime();
+
+          if (decodedToken.exp * 1000 < clientTime) {
+            handleLogout();
+          } else {
+            setAuth(decodedToken);
+          }
+        } catch (error) {
+          console.error('Invalid token:', error);
+          handleLogout();
+        }
+      } else {
+        setAuth(null);
+      }
+      setLoading(false);
+    };
+    checkAuth();
+  }, []);
+
+  const handleSetAuth = (authData) => {
+    const token = authData;
+    try {
+      const decodedToken = jwtDecode(token);
+      setAuth(decodedToken);
+      if (decodedToken.isFirstConnexion) {
+        setFirstAuth(true);
+      }
+      localStorage.setItem('authToken', token);
+    } catch (error) {
+      console.error('Error decoding token:', error);
+    }
+  };
+
+  const handleLogout = () => {
+    setAuth(null);
+    localStorage.removeItem('authToken');
+    setIsLoggedOut(true);  // Update state to trigger navigation
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Router>
+      {isLoggedOut && window.location.pathname !== "/sign" && <Navigate to="/sign" />}
       <div>
         {/* Main Content */}
         <div className="container mt-4">
           <Routes>
-            <Route path="/login" element={<Sign />} />
-            {routeConfig.map((route, index) => (
+            {!auth && <Route path='/' element={<Home />} />}
+            <Route path="/sign"
+              element={auth ? <Navigate to='/' /> : <Sign setAuth={handleSetAuth} unsetLoggedOut={setIsLoggedOut} />}
+            />
+            {auth && routeConfig.map((route, index) => (
               <Route key={index} path={route.path} element={<Dashboard Content={route.content} />} />
             ))}
+            <Route path="/logout" element={<Logout onLogout={handleLogout} />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
---
+++ client/src/components/Logout.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+
+const Logout = ({ onLogout }) => {
+  useEffect(() => {
+    onLogout(); // Call the logout function when this component mounts.
+  }, [onLogout]);
+
+  return <Navigate to="/sign" />; // Redirect to login page after logging out.
+};
+
+export default Logout;
