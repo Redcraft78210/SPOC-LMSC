ede17e499445a779945ed0a57468b456bf4e46f9
jean FLo <rrtracer@gmail.com>
2025-03-21 10:46:29 +0100

    actual state save

--- client/src/pages/Professeur/DashboardProf.jsx
+++ client/src/pages/Professeur/DashboardProf.jsx
@@ -2,34 +2,61 @@ import React from "react";
 import NavBar from "../../components/ProfComp/NavBar";
 import Darkmode from "../../components/ProfComp/Darkmode";
 import Card from "../../components/ProfComp/Card";
-
+import Darkmode2 from "../../components/ProfComp/Darkmode2";
+import OnAir from "../../components/OnAir";
+import OnAir2 from "../../components/ProfComp/OnAir2";
 const DashboardProf = () => {
   return (
-    <div className="w-11/12">
-      <div className="grid grid-cols-12 grid-rows-12 gap-2">
-        <div className="row-span-12 h-full  w-fit">
+    <div className="w-full h-full">
+      <div className="absolute h-fit w-5/6 top-2 left-32 flex flex-row justify-between ">
+        <h1 className=" text-5xl text-blue-700 font-bold w-fit h-fit ">
+          SPOC LMSC 218
+        </h1>
+        <p className="w-fit -mt-6">
+          <Darkmode2 />
+        </p>
+      </div>
+      {/* <div className="row-start-1 row-span-2 flex ">
+        <div className="mt-16 w-1/3 ">
+          <div className="h-full w-fit  ">
+            <NavBar />
+          </div>
+        </div>
+        <div className="w-2/3 grid grid-cols-3 grid-rows-3 gap-2 grid-auto-rows-[minmax(50px,_auto)] g-4">
+          <div className="col-start-1 row-start-1">
+            <Darkmode />
+          </div>
+          <div className="col-start-2 row-start-1">
+            <Card className={"h-full"} content={"13"} />
+          </div>
+          <div className="row-start-2 col-span-2">
+            <Card className={"w-full h-full"} content={"14"} />
+          </div>
+          <div className="row-start-3 col-span-2">
+            <Card className={"w-full h-full"} content={"16"} />
+          </div>
+          <div className="row-span-3 col-start-3 row-start-1">
+            <Card className={"w-full h-full"} content={"11"} />
+          </div>
+        </div>
+      </div> */}
+
+      <div className="flex border border-red-500 mt-[4rem] md:w-screen ">
+        <div className="border border-lime-300 w-1/12 h-screen">
           <NavBar />
         </div>
-        <p className="col-span-11 text-5xl   text-blue-700 backdrop-blur-3xl w-fit h-fit">
-          SPOC LMSC 218
-        </p>
-        <div className=" col-span-11 row-span-11 col-start-2 row-start-2">
-          <div className="grid grid-cols-3 grid-rows-3 gap-2 grid-auto-rows-[minmax(50px,_auto)]">
-            <div className="col-start-1 row-start-1">
-              <Darkmode />
+        {/* dashboard */}
+        <div className="border border-blue-600  w-11/12">
+          {/* GRID du dashboard */}
+          <div class="grid grid-cols-3 xl:grid-cols-3 gap-4 px-4 py-4">
+            <div class="w-full h-48 rounded-xl">
+              <OnAir2/>
             </div>
-            <div className="col-start-2 row-start-1">
-              <Card className={"h-full"} content={"13"} />
-            </div>
-            <div className="row-start-2 col-span-2">
-              <Card className={"w-full h-full"} content={"14"} />
-            </div>
-            <div className="row-start-3 col-span-2">
-              <Card className={"w-full h-full"} content={"16"} />
-            </div>
-            <div className="row-span-3 col-start-3 row-start-1">
-              <Card className={"w-full h-full"} content={"11"} />
-            </div>
+            <div class="w-full h-48  bg-blue-600 rounded-xl">5</div>
+            <div class="w-full h-48  bg-blue-900 rounded-xl col-span-2">3</div>
+            <div class="w-full row-start-1 col-start-3 bg-blue-600 rounded-xl row-span-3"></div>
+            <div class="w-full h-48 bg-blue-900 rounded-xl ">2</div>
+            <div class="w-full h-48 bg-blue-900 rounded-xl">6</div>
           </div>
         </div>
       </div>
