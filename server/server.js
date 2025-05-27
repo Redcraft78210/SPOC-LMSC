9033e2a205fdd2d765a9f667484d78d460317e57
Redcraft78210 <belaise.clement@gmail.com>
2025-05-27 13:08:59 +0200

    fix: Update API URLs and allowed origins for local development

--- server/server.js
+++ server/server.js
@@ -84,7 +84,8 @@ const DEFAULTS = {
 // En production, il faut ajouter le nom de domaine de l'application
 // const allowedOrigins = ["https://localhost:5173", "https://localhost:8443", "https://your-production-domain.com"];
 
-const allowedOrigins = ["https://192.168.36.150"];
+const allowedOrigins = ["https://192.168.36.150",
+  "https://localhost"];
 
 // Middleware
 app.use(cors({
